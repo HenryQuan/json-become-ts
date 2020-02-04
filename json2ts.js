f2aa98d236e9888270d06b89ab31084afd1f0632
Yiheng <development.henryquan@gmail.com>
2020-02-04 20:24:01 +1100

    add ts writer

--- json2ts.js
+++ json2ts.js
@@ -1,3 +1,22 @@
 const fs = require('fs');
 
-fs.writeFileSync('test.md', '# Hello World');
+const argv = process.argv.slice(2);
+if (argv.length === 0) {
+  showUsage();
+} else {
+  const jsonPath = argv[0];
+
+  const file = fs.readFileSync(jsonPath);
+  const jsonObject = JSON.parse(file);
+  for (const key in jsonObject) {
+    console.log(key, typeof jsonObject[key]);
+  }
+}
+
+/**
+ * Show usage and exit the process
+ */
+function showUsage() {
+  console.log('Usage: node json2ts.js [json_path] [folder_name]');
+  process.exit();
+}
