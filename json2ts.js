58efdddd079fd7b6652351ab9cfda53363f84a4d
Yiheng <development.henryquan@gmail.com>
2020-02-04 22:06:43 +1100

    at least it generates something

--- json2ts.js
+++ json2ts.js
@@ -8,9 +8,45 @@ if (argv.length === 0) {
 
   const file = fs.readFileSync(jsonPath);
   const jsonObject = JSON.parse(file);
-  for (const key in jsonObject) {
-    console.log(key, typeof jsonObject[key]);
+  
+  const rootName = getJsonFileName(jsonPath);
+  convertR(jsonObject, rootName, rootName);
+}
+
+/**
+ * Convert recursively
+ * @param {any} object any json object
+ * @param {string} path current path
+ * @param {string} name used for class name
+ */
+function convertR(object, path, name) {
+  // Create a folder if doesn't exist to start conversion
+  if (!fs.existsSync(path)) fs.mkdirSync(path);
+  const className = upperFirst(name);
+
+  const fileContent = [`interface ${className} {`];
+  // Loop through object recursively and create new files and folders
+  for (const key in object) {
+    const type = typeof object[key];
+    console.log(type);
+    if (type === 'object') {
+      // add new line
+      fileContent.push(`  ${key}: ${upperFirst(key)}`);
+
+      const newPath = path.split('/');
+      newPath.push(key);
+      const finalPath = newPath.join('/');
+      // Go deeper
+      convertR(object[key], finalPath, key);
+    } else {
+      // create new files
+      fileContent.push(`  ${key}: ${type}`);
+    }
   }
+
+  fileContent.push('}\n');
+  fileContent.push(`export { ${className} }`);
+  fs.writeFileSync(path + '.ts', fileContent.join('\n'));
 }
 
 /**
@@ -20,7 +56,16 @@ if (argv.length === 0) {
 function getJsonFileName(jsonPath) {
   const temp = jsonPath.split('/');
   return temp[temp.length - 1].replace('.json', '');
-} 
+}
+
+/**
+ * Make the first char uppercase
+ * - from https://stackoverflow.com/a/1026087
+ * @param {string} name 
+ */
+function upperFirst(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
 
 /**
  * Show usage and exit the process
