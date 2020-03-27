9cbe26c8d4d3898098e1fa89f488f76918deb5f0
Yiheng <development.henryquan@gmail.com>
2020-03-27 17:33:49 +1100

    more functions are added

--- GUI/lib/core/Writter.dart
+++ GUI/lib/core/Writter.dart
@@ -4,15 +4,29 @@ import 'dart:convert';
 abstract class Writter {
   /// It can either be a `list` or a `map` or null
   dynamic json;
+  /// There are different files all in one place
+  List<List<String>> _classes;
 
   Writter(String jsonString) {
     // Decode it into a map
     try {
       json = jsonDecode(jsonString);
+      _convert(json);
     } catch (e) {
       // JSON is not valid
       print('JSON is not valid');
     }
+  }
+
+  /// Merge all files into one
+  String toString() {
+    // Map each list into files and join files into the final output
+    return _classes.map((c) => c.join('\n')).join('\n\n');
+  }
+
+  /// Convert json into any language
+  _convert(dynamic object) {
+
   }
 
   /// Convert dart types to other types
